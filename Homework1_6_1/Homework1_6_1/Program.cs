8cfb89296bee92f6704a3b5c84d1f29b7d80dabc
PavelOotsutsuki <109985069+PavelOotsutsuki@users.noreply.github.com>
2022-12-21 01:17:52 +0300

    v1

---
+++ Homework1_6_1/Homework1_6_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1_6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player("Аркадий", 1000, 10, 50, 0);
+            player1.ShowStats();
+        }
+    }
+
+    class Player
+    {
+        private string _name;
+        private int _health;
+        private int _armor;
+        private int _damage;
+        private int _mana;
+
+        public Player(string name, int health, int armor, int damage, int mana)
+        {
+            _name = name;
+            _health = health;
+            _armor = armor;
+            _damage = damage;
+            _mana = mana;
+        }
+
+        public void ShowStats()
+        {
+            Console.WriteLine("Игрок " + _name + ":");
+            Console.WriteLine();
+            Console.WriteLine("Здоровье: " + _health + "\nБроня: " + _armor + "\nУрон: " + _damage + "\nМана: " + _mana);
+        }
+    }
+}
+
